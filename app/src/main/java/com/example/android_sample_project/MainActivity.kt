b0403113b5d8ae6f9328621eef9aa627dd72bf98
Abhishek <63782877+ABHISHEK22415@users.noreply.github.com>
2025-08-02 16:33:24 -0700

    Using Jetpack Compose

--- app/src/main/java/com/example/android_sample_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sample_project/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_sample_project.ui.theme.Android_Sample_projectTheme
@@ -24,6 +28,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    test(name = "Hello")
                 }
             }
         }
@@ -36,12 +41,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
+}
+
+
+@Composable
+fun test (name: String)
+{
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+    Button(onClick = {}) {
+        Text(
+            text = name
+        )
+    }
+    }
 }
 
 @Preview(showBackground = true)
-@Composable
+@Composable // using  the prime logic for this code  and try the feature sof the jetpack compose
 fun GreetingPreview() {
     Android_Sample_projectTheme {
-        Greeting("Android")
+        Greeting("Andr")
     }
 }
