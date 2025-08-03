5f0679f364a440df070630d35d78761db80e2ee3
Abhishek <63782877+ABHISHEK22415@users.noreply.github.com>
2025-08-03 16:13:42 -0700

    Basic_Counter_Activity

--- app/src/main/java/com/example/android_sample_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sample_project/MainActivity.kt
@@ -4,17 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.android_sample_project.ui.theme.Android_Sample_projectTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,48 +39,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Android_Sample_projectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    test(name = "Hello")
-                }
+
+                var count  by remember{ mutableStateOf(0) }
+
+               Column(
+                   modifier = Modifier.fillMaxSize().padding(30.dp),
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally
+
+
+               ) {
+                   Text(text = count.toString(),
+                       fontSize= 30.sp
+                   )
+
+                   Button(onClick ={
+                       count ++
+                   } ) {
+                        // scopevalue of button
+
+                       Text("Click Me")
+                   }
+               }
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 
-}
+// understanding the state management in the COmpose
 
 
-@Composable
-fun test (name: String)
-{
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-    Button(onClick = {}) {
-        Text(
-            text = name
-        )
-    }
-    }
-}
 
-@Preview(showBackground = true)
-@Composable // using  the prime logic for this code  and try the feature sof the jetpack compose
-fun GreetingPreview() {
-    Android_Sample_projectTheme {
-        Greeting("Andr")
-    }
-}
+
