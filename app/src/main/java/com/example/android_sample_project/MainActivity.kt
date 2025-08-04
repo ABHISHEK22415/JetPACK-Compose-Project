48bec81c14ae90126ec1701bd031b3aaf437a633
Abhishek <63782877+ABHISHEK22415@users.noreply.github.com>
2025-08-03 17:00:41 -0700

    Building User input and showing user using lazy loading

--- app/src/main/java/com/example/android_sample_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sample_project/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android_sample_project
 
 import android.os.Bundle
+import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +11,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,27 +46,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             Android_Sample_projectTheme {
 
-                var count  by remember{ mutableStateOf(0) }
+                var textState= remember { mutableStateOf("") }
 
-               Column(
-                   modifier = Modifier.fillMaxSize().padding(30.dp),
-                   verticalArrangement = Arrangement.Center,
-                   horizontalAlignment = Alignment.CenterHorizontally
+                var textList= remember { mutableStateOf(listOf<String>()) }
+                //need proper padding and proper UI background theme for this
+
+                Column (modifier = Modifier.fillMaxSize().padding(16.dp))
+                {
+                    Row (modifier = Modifier.fillMaxWidth()){
+
+                       OutlinedTextField(value=textState.value,
+                           onValueChange = {newText-> textState.value=newText},
+                           label={ Text("Enter your text name ") },)
+
+                        Button(onClick = {textList.value = textList.value + textState.value })
+                        {
+                            Text("Submit")
+                        }
 
 
-               ) {
-                   Text(text = count.toString(),
-                       fontSize= 30.sp
-                   )
 
-                   Button(onClick ={
-                       count ++
-                   } ) {
-                        // scopevalue of button
 
-                       Text("Click Me")
-                   }
-               }
+
+                    }
+                    LazyColumn { items(textList.value)
+                    {currentText ->  Text(text = currentText)
+                    }
+                    }
+
+
+                }
+
+
 
 
             }
